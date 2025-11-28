4ca6732b23544b20c1db69612e5158136ba0d071
QuentinB <quentin.bourdois@supdevinci-edu.fr>
2025-11-28 10:58:44 +0100

    Added FantasyTheme.css and updated PlayView.css

--- frontend/src/components/PlayView.js
+++ frontend/src/components/PlayView.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/PlayView.css';
+import '../styles/FantasyTheme.css';
 import RatingSection from './RatingSection';
 import ReportModal from './ReportModal';
 
